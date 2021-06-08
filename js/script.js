d5172bacd0221e2f53a52a4fdfa8c3d2ce96a8bb
NicolaRebecca <master9895@gmail.com>
2021-06-08 12:49:07 +0200

    script js e layout completi

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+
+
 var createSum = document.getElementById ('calculate');
 var resultSumElement = document.getElementById ('totale');
+alert("Benvenuto! usa il codice promo JUNE20 per ricevere il 20% di sconto")
 
 createSum.addEventListener('click', function() {
 
@@ -12,6 +15,24 @@ for (var i = 0; i < ceckNum.length; i++){
     }
 }
 
-resultSumElement.innerHTML = parseInt("50") + resultSumInt;
+    resultSumElement.innerHTML = 50 + parseInt(resultSumInt);
+
+    var discount = document.getElementById("coupon").value;
+    var fullprice = 50;
+    if(discount === "JUNE20"){
+        var discountValue = (fullprice / 100) * 20;
+        resultSumElement.innerHTML = fullprice + resultSumInt - discountValue ;
+    }
+
 
 })
+
+
+
+
+
+// var totalPrice = resultSumInt + (resultSumInt * sale);
+
+
+
+
